6a8eef8e57e359afa534f1fa85abe63e4b34f255
Phivos Papapanagiotakis <csd3823@csd.uoc.gr>
2020-12-29 13:47:03 +0200

    Moved FilterGridMotion() and some vars from private to protected in Level2.h. Moved #defines from Level2.cpp to Level.h - now they can be seens from Level3 aswell. Copied previous movement lambda in the new lambda (for now).

--- ConsoleApplication2/UnitTests/UnitTest2/Level2.h
+++ ConsoleApplication2/UnitTests/UnitTest2/Level2.h
@@ -2,6 +2,10 @@
 #include "Level1.h"
 #include <allegro5/allegro_primitives.h>
 
+#define RECT_MAX_SPEED_X 120 // pixels per second. (p/s)
+#define RECT_MAX_SPEED_Y 120  // pixels per second. (p/s)
+#define RECT_UPDATE_POS 0.00834 // 120 times per second.
+
 struct Movement {
 	double x_speed, y_speed;
 };
@@ -9,28 +13,28 @@ struct Movement {
 class UnitTest2 : protected UnitTest {
 	ALLEGRO_COLOR color;
 	ALLEGRO_EVENT_QUEUE* keyboard_rect_queue;
-	
 	ALLEGRO_TIMER* rect_pos_timer;
-	Rect rectangle{224, 176, 16, 16 };
-	Movement rect_mvmnt{ 0,0 };
-	bool movement_keys[ALLEGRO_KEY_MAX] = { 0 };
+	
 	std::vector<std::vector<byte>> grid; //grid[row][col] 
 	bool rect_filled = true;
 
 	void ReadTextGrid(std::vector<std::vector<byte>>&, Dim&);
-	void FilterGridMotion(const Rect&, int&, int&);
 	void FilterGridMotionLeft(const Rect&, int&);
 	void FilterGridMotionRight(const Rect&, int&);
 	void FilterGridMotionUp(const Rect&, int&);
 	void FilterGridMotionDown(const Rect&, int&);
 	bool CanPassGridTile(uint, uint, byte);
 
-
 protected:
+	Movement rect_mvmnt{ 0,0 };
+	Rect rectangle{ 224, 176, 16, 16 };
+	bool movement_keys[ALLEGRO_KEY_MAX] = { 0 };
 	ALLEGRO_EVENT_QUEUE* timer_queue0;
 	std::function<void(void)> render_rect;
 	std::function<void(void)> input_rect;
 	std::function<void(void)> physics_rect;	
+
+	void FilterGridMotion(const Rect&, int&, int&);
 
 public:
 	UnitTest2();
--- ConsoleApplication2/UnitTests/UnitTest3/Level3.cpp
+++ ConsoleApplication2/UnitTests/UnitTest3/Level3.cpp
@@ -1,7 +1,32 @@
 #include "Level3.h"
 
 UnitTest3::UnitTest3(){
+	ALLEGRO_EVENT timer_event;
+	mario_physics = [&] {
+		if (al_get_next_event(timer_queue0, &timer_event)) {
+			rect_mvmnt.y_speed = 0;
+			rect_mvmnt.x_speed = 0;
+		if (movement_keys[ALLEGRO_KEY_W]) {
+			rect_mvmnt.y_speed -= RECT_MAX_SPEED_Y;
+		}
+		if (movement_keys[ALLEGRO_KEY_S]) {
+			rect_mvmnt.y_speed += RECT_MAX_SPEED_Y;
+		}
+		if (movement_keys[ALLEGRO_KEY_A]) {
+			rect_mvmnt.x_speed -= RECT_MAX_SPEED_X;
+		}
+		if (movement_keys[ALLEGRO_KEY_D]) {
+			rect_mvmnt.x_speed += RECT_MAX_SPEED_X;
+		}
 
+		int dx, dy;
+		dx = rect_mvmnt.x_speed * RECT_UPDATE_POS;
+		dy = rect_mvmnt.y_speed * RECT_UPDATE_POS;
+		FilterGridMotion(rectangle, dx, dy);
+		rectangle.x += dx;
+		rectangle.y += dy;
+	}
+};
 }
 
 void UnitTest3::Initialise(void) {
@@ -16,6 +41,7 @@ void UnitTest3::Load(void) {
 	game.PushbackInput(input_rect);
 	//game.PushbackInput(input_scroll);
 	//game.PushbackPhysics(physics_rect);
+	game.PushbackPhysics(mario_physics);
 }
 
 void UnitTest3::Clear(void) {
