f0a84e182939fb15fb9c322e49b11e647f856879
Labis Anargyrou <L4biscs@gmail.com>
2020-12-31 01:32:36 +0200

    Recreation of class AnimationFilm cause of VS bug

--- ConsoleApplication2/UnitTests/UnitTest3/Level3.cpp
+++ ConsoleApplication2/UnitTests/UnitTest3/Level3.cpp
@@ -39,7 +39,7 @@ UnitTest3::UnitTest3(){
 	};
 
 	
-	// DUMMY CODE
+	// DUMMY CODE plz
 	auto animations = map_info_parser.GetList("ANIMATIONS");
 	ConfigParser coin_parser;
 	ConfigParser q_mark_parser;
