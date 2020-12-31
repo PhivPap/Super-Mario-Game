d96ec23481f92fed40dc0497b7c5524f52a79750
Labis Anargyrou <L4biscs@gmail.com>
2020-12-31 02:37:35 +0200

    Add source for animation classes

--- ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.cpp
+++ ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.cpp
@@ -1,1 +1,24 @@
 #include "AnimationFilmHolder.h"
+
+AnimationFilmHolder::AnimationFilmHolder() {}
+AnimationFilmHolder::~AnimationFilmHolder() { CleanUp(); }
+
+const AnimationFilmHolder& AnimationFilmHolder::Get(void) {
+	return holder;
+}
+
+/* Load all films */
+void AnimationFilmHolder::LoadAll(const std::string& text) {
+
+}
+
+void AnimationFilmHolder::CleanUp(void) {
+	for (auto& i : films)
+		delete(i.second);
+	films.clear();
+}
+
+const AnimationFilm* AnimationFilmHolder::GetFilm(const std::string& id) const {
+	auto i = films.find(id);
+	return i != films.end() ? i->second : nullptr;
+}
--- ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.h
+++ ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.h
@@ -1,5 +1,19 @@
 #pragma once
-class AnimationFilmHolder
-{
-};
+#include <map>
+#include <string>
+#include "AnimationFilm.h"
 
+class AnimationFilmHolder {
+	using Films = std::map<std::string, AnimationFilm*>;
+	Films films;
+	//BitmapLoader bitmaps; // only for loading of film bitmaps
+	static AnimationFilmHolder holder; // singleton
+	AnimationFilmHolder(void);
+	~AnimationFilmHolder();
+
+public:
+	static const AnimationFilmHolder& Get(void);
+	void LoadAll(const std::string&);
+	void CleanUp(void);
+	const AnimationFilm* GetFilm(const std::string&) const;
+};
