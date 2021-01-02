0fa2605a77fd4400e233ace2d158cd7c610c52a6
Labis Anargyrou <L4biscs@gmail.com>
2021-01-02 13:50:24 +0200

    Revert "Design fix"
    This reverts commit 6280bb9c2dc434c692b92a30fa94f0845393e5b0.

--- ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.cpp
+++ ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.cpp
@@ -1,28 +1,15 @@
 #include "AnimationFilmHolder.h"
 
-AnimationFilmHolder AnimationFilmHolder::holder; // definition
-
 AnimationFilmHolder::AnimationFilmHolder() {}
 AnimationFilmHolder::~AnimationFilmHolder() { CleanUp(); }
 
-/* Load all films from source_path(.data) using parser */
-void AnimationFilmHolder::LoadAll(const char* path, const Parser& parser) {
-	std::list<AnimationFilm::Data> output;
-	parser(output, path);
-	assert(!output.empty());
+const AnimationFilmHolder& AnimationFilmHolder::Get(void) {
+	return holder;
+}
 
-	// validation
-	for (auto i : output) {
-		std::cout << "id: " << i.id << ", path: " << i.path << "\nrects: ";
-		for (auto j : i.rects)
-			std::cout << "[" << j.x << "," << j.y << "," << j.w << "," << j.h << "], ";
-		std::cout << std::endl << std::endl;
-	}
+/* Load all films */
+void AnimationFilmHolder::LoadAll(const std::string& text) {
 
-	for (auto& entry : output) {
-		assert(!GetFilm(entry.id));
-		films[entry.id] = new AnimationFilm(bitmaps.Load(entry.path), entry.rects, entry.id);
-	}
 }
 
 void AnimationFilmHolder::CleanUp(void) {
@@ -34,8 +21,4 @@ void AnimationFilmHolder::CleanUp(void) {
 const AnimationFilm* AnimationFilmHolder::GetFilm(const std::string& id) const {
 	auto i = films.find(id);
 	return i != films.end() ? i->second : nullptr;
-}
-
-const BitmapLoader& AnimationFilmHolder::GetBitmapLoader(void) const {
-	return bitmaps;
 }
