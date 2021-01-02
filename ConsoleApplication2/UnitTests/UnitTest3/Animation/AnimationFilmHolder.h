6280bb9c2dc434c692b92a30fa94f0845393e5b0
Labis Anargyrou <L4biscs@gmail.com>
2021-01-02 13:46:03 +0200

    Design fix

--- ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.h
+++ ConsoleApplication2/UnitTests/UnitTest3/Animation/AnimationFilmHolder.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <map>
 #include <string>
+#include <list>
+#include "BitmapLoader.h"
 #include "AnimationFilm.h"
 
 class AnimationFilmHolder {
+public:
+	using Parser = std::function<
+		void(std::list <AnimationFilm::Data>& output, const char* input)
+	>;
+
+private:
 	using Films = std::map<std::string, AnimationFilm*>;
-	Films films;
-	//BitmapLoader bitmaps; // only for loading of film bitmaps
-	static AnimationFilmHolder holder; // singleton
+	Films			films;
+	BitmapLoader	bitmaps; // only for loading of film bitmaps
+	static AnimationFilmHolder	holder; // singleton
 	AnimationFilmHolder(void);
 	~AnimationFilmHolder();
 
 public:
-	static const AnimationFilmHolder& Get(void);
-	void LoadAll(const std::string&);
+	static inline AnimationFilmHolder& Get(void) { return holder; }
+	void LoadAll(const char*, const Parser&); 
 	void CleanUp(void);
 	const AnimationFilm* GetFilm(const std::string&) const;
+	const BitmapLoader& GetBitmapLoader(void) const;
 };
