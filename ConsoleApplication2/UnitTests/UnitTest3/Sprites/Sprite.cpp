4ab58d5cb2579cf55ef49828b420ea22f499e309
Phivos Papapanagiotakis <csd3823@csd.uoc.gr>
2021-01-03 12:24:47 +0200

    Added Sprite Class

---
+++ ConsoleApplication2/UnitTests/UnitTest3/Sprites/Sprite.cpp
@@ -0,0 +1,71 @@
+#include "Sprite.h"
+
+void Sprite::SetMover(const Mover& f) {
+	quantizer.SetMover(mover = f); // <-- ... ok dude
+}
+
+const Rect Sprite::GetBox(void) const {
+	return { x, y, frame_box.w, frame_box.h }; // do we need x,y ??
+}
+
+void Sprite::Move(int dx, int dy) {
+	quantizer.Move(GetBox(), &dx, &dy); // why pass pointer?
+}
+
+void Sprite::SetPos(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+void Sprite::SetZorder(uint z) {
+	z_order = z;
+}
+
+uint Sprite::GetZorder(void) {
+	return z_order;
+}
+
+void Sprite::SetFrame(byte i) {
+	if (i != frame_no) {
+		assert(i < curr_film->GetTotalFrames());
+		frame_box = curr_file->GetFrameBox(frame_no = i);
+	}
+}
+
+byte Sprite::GetFrame(void) const {
+	return frame_no;
+}
+
+void Sprite::SetBoundingArea(const BoundingArea& area) {
+	assert(!bounding_area); 
+	bounding_area = area.Clone();
+}
+
+void Sprite::SetBoundingArea(BoundingArea* area) {
+	assert(!bounding_area);
+	bounding_area = area;
+}
+
+const BoundingArea* Sprite::GetBoundArea(void) const {
+	return bounding_area;
+}
+
+const std::string& Sprite::GetTypeId(void) {
+	return type_id;
+}
+
+void Sprite::SetVisibility(bool v) {
+	is_visible = v;
+}
+
+bool Sprite::isVisible(void) const {
+	return is_visible;
+}
+bool Sprite::CollisionCheck(const Sprite*) const {
+	assert(0);
+	//TODO
+}
+void Sprite::Display(ALLEGRO_BITMAP, const Rect&, const Clipper&) const {
+	assert(0);
+	//TODO
+}
---
+++ ConsoleApplication2/UnitTests/UnitTest3/Sprites/Sprite.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <functional>
+#include "Types.h"
+#include "AnimationFilm.h"
+
+
+class Sprite {
+
+
+public:
+	using Mover = std::function<void(const Rect&, int* dx, int* dy)>;
+protected:
+	byte frame_no = 0;
+	Rect frame_box;
+	int x = 0, y = 0;
+	bool is_visible = false;
+	AnimationFilm* curr_film = nullptr;
+	BoundingArea* bounding_area = nullptr;
+	uint z_order = 0;
+	std::string type_id, state_id;
+	Mover mover;
+	MotionQuantizer quantizer;
+public:
+	void SetMover(const Mover&);
+	const Rect GetBox(void) const;
+	void Move(int, int);
+	void SetPos(int, int);
+	void SetZorder(uint);
+	uint GetZorder(void);
+	void SetFrame(byte);
+	byte GetFrame(void) const;
+	void SetBoundingArea(const BoundingArea&);
+	void SetBoundingArea(BoundingArea*);
+	const BoundingArea* GetBoundArea(void) const;
+	const std::string& GetTypeId(void);
+	void SetVisibility(bool);
+	bool isVisible(void) const;
+	bool CollisionCheck(const Sprite*) const;
+	void Display(ALLEGRO_BITMAP, const Rect&, const Clipper&) const;
+};
+
